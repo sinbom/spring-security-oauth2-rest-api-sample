1d535d7b7b2589207ff5332d22888d2c9851968f
syj <nmrhtn7898@naver.com>
2020-05-19 17:54:20 +0900

    add querydsl dependencies and plugin in resc module

--- resc/src/main/java/me/nuguri/resc/config/ApplicationConfiguration.java
+++ resc/src/main/java/me/nuguri/resc/config/ApplicationConfiguration.java
@@ -1,9 +1,12 @@
 package me.nuguri.resc.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+
+import javax.persistence.EntityManager;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -16,6 +19,11 @@ public class ApplicationConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory(EntityManager entityManager) {
+        return new JPAQueryFactory(entityManager);
     }
 
 }
