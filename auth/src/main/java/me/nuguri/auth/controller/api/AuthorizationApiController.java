318741191147469c9b1b58b43e8dfa3345064362
SYJ AT HOME <nmrhtn7898@naver.com>
2020-05-29 22:36:31 +0900

    polish

--- auth/src/main/java/me/nuguri/auth/controller/api/AuthorizationApiController.java
+++ auth/src/main/java/me/nuguri/auth/controller/api/AuthorizationApiController.java
@@ -28,16 +28,22 @@ public class AuthorizationApiController {
 
     private final ModelMapper modelMapper;
 
-    /**
-     * Authorization Header 통해 전달 받은 Bearer 토큰이 유효하면 만료시킨다.
-     * @param accessToken 엑세스 토큰
-     * @return
-     */
-    @PostMapping(value = "/oauth/revoke_token", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> revokeToken(@Oauth2AccessToken DefaultOAuth2AccessToken accessToken) {
-        tokenStore.removeAccessToken(accessToken);
-        return ResponseEntity.ok(accessToken);
-    }
+//  jwt 방식의 토큰 (header.payload.signature)은 서버에서 별도로 저장하지 않아 stateless하며 토큰 자체에서 정보를 가진다
+//  header.payload의 값을 base64 디코딩한 값과 signature 값을 헤더에 존재하는 해싱 알고리즘과 개인키(비밀키)로
+//  디코딩한 값인 header.payload가 일치하고(일치하다는 것은 누군가 임의로 변경하지 않았다는 뜻, 변경하려면 개인키로 해싱해야되는데 개인키를 모르면 못함)
+//  토큰내 정보인 만료 시간을 지나지 않았으면 그 자체로 유효하기 때문에 시간이 지나서 만료되는 경우가 아니라면 토큰을 만료시킬 수 없다
+//  토큰의 존재 자체와 header.payload = signature의 일치와 만료 시간이 넘지 않은 것 자체가 유효한 것이기 때문
+
+//    /**
+//     * Authorization Header 통해 전달 받은 Bearer 토큰이 유효하면 만료시킨다.
+//     * @param accessToken 엑세스 토큰
+//     * @return
+//     */
+//    @PostMapping(value = "/oauth/revoke_token", produces = MediaType.APPLICATION_JSON_VALUE)
+//    public ResponseEntity<?> revokeToken(@Oauth2AccessToken DefaultOAuth2AccessToken accessToken) {
+//        tokenStore.removeAccessToken(accessToken);
+//        return ResponseEntity.ok(accessToken);
+//    }
 
     /**
      * Authorization Header 통해 전달 받은 Bearer 토큰으로 토큰을 발급 받은 계정 정보 조회
